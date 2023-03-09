cdd0927b9d69c9fd29ccedd3a5b7b78d16fff65c
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-08 21:31:02 -0300

    feat: start templates
    creates directoris and first template file, still for testing

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "todo/list.html")
