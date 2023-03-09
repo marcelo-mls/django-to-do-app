04f57ddc037cbb56fbaf1cd84b5628b64cb793ff
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-09 00:04:59 -0300

    feat: configure form
    configure POST method for tasks form and also set redirection

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 from . import models
+from . import forms
 
 
 def index(request):
     tasks = models.Task.objects.all()
-    context = {"tasks": tasks}
+    form = forms.TaskForm()
+
+    if request.method == "POST":
+        form = forms.TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {"tasks": tasks, "form": form}
 
     return render(request, "todo/list.html", context)
