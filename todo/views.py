5c9ffc131598e765ab443ecba74ee0cf715e4d69
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-08 22:22:43 -0300

    feat: render tasks
    renders all tasks from database on html file

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+from . import models
+
 
 def index(request):
-    return render(request, "todo/list.html")
+    tasks = models.Task.objects.all()
+    context = {"tasks": tasks}
+
+    return render(request, "todo/list.html", context)
