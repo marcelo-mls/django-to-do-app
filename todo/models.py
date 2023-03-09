cff63a2dbc15826a44e594bc5cfe00461b085ae8
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-08 21:52:11 -0300

    feat(start-models): start the Taks model and register it in 'admin.py'
    n

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=45)
+    isFinished = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - since: {self.created_at.date()}"
