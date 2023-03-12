6f71ec479c78ed7f63e18d7f75dd7b72f2e6df17
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-12 19:56:36 -0300

    feat: create CRUD
    create a complete CRUD with django rest framework

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path  # , include
 # from rest_framework import routers
 
-from todo.views import apiOverview  # , TaskViewSet
+import todo.views as views
 
 # router = routers.DefaultRouter()
 # router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path("", apiOverview),
+    path("", views.apiOverview, name="api-overview"),
+    path("task-list/", views.taskList, name="task-list"),
+    path("task-details/<str:pk>", views.taskDetails, name="task-details"),
+    path("task-create/", views.taskCreate, name="task-create"),
+    path("task-update/<str:pk>", views.taskUpdate, name="task-update"),
+    path("task-delete/<str:pk>", views.taskDelete, name="task-delete"),
     # path("api/", include(router.urls))
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 # from rest_framework import viewsets
 
 from todo.models import Task
+from todo.serializers import TaskSerializer
+
+# class TaskViewSet(viewsets.ModelViewSet):
+#     queryset = Task.objects.all()
 
 
 @api_view(["GET"])
@@ -14,8 +19,50 @@ def apiOverview(request):
         "Update": "/task-update/<str:pk>/",
         "Delete": "/task-delete/<str:pk>/",
     }
+
     return Response(api_urls)
 
 
-# class TaskViewSet(viewsets.ModelViewSet):
-#     queryset = Task.objects.all()
+@api_view(["GET"])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def taskDetails(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response(f'Task {pk} deleted')
